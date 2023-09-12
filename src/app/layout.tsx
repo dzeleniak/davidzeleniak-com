7767ce9ca5f027d0850a5d9d2f3682b3d73620a4
dzeleniak <zeleniak.david@gmail.com>
2023-09-12 13:52:41 -0700

    initialize dev branch for site

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - David Zeleniak',
+    default:
+      'David Zeleniak - Software Developer, Automation Engineer, and Musician.',
+  },
+  description:
+    "I'm David, a software engineer based out of Las Vegas. I'm currently working for Kimberly Clark as a Full Stack Engineer on the innovation team, where we create prototypes for new initiatives using emerging technology..",
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
+ 
